6ee43b20fd92efd8a0cdbc286bc19d4528725010
Jorge Takeshi Sato <jsato@cit>
2020-06-22 02:48:39 -0300

    Refactor

--- src/NumberToWord.java
+++ src/NumberToWord.java
@@ -1,15 +1,47 @@
-
-
 public class NumberToWord {
 
     private static final String[] units = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-
     private static final String[] doubles = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-
     private static final String[] tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
     private static final String[] hundreds1 = {"", "thousand", "million", "billion"};
     private static final String[] hundreds2 = {"", "thousands", "millions", "billions"};
+
+    private static String convertUnderOneThousand(int number) {
+        String word = "";
+        int index = number % 100;
+
+        if (index < 10) {
+            word += units[index % 10];
+
+        } else if (index < 20) {
+            word += doubles[index % 10];
+        } else {
+            if (index % 10 == 0) {
+                word += tens[index / 10];
+            } else {
+                word += tens[index / 10] + "-" + units[index % 10];
+            }
+        }
+        int hundred = number / 100;
+        return hundred > 0 ? units[hundred] + " hundred " + word : word;
+    }
+
+    // O(n) — Linear Time
+    private static String wordToNumber(int number) {
+        String word = "";
+        int index = 0;
+        do {
+            int num = number % 1000;
+            if (num != 0) {
+                String singularPlural = num == 1 ? hundreds1[index] : hundreds2[index];
+                word = convertUnderOneThousand(num) + " " + singularPlural + " " + word;
+            }
+            index++;
+            number = number / 1000;
+        } while (number > 0);
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
 
     public static void main(String[] args) {
 
@@ -22,53 +54,5 @@ public class NumberToWord {
         System.out.println("  6666666: " + wordToNumber(6666666));
         System.out.println(" 77777777: " + wordToNumber(77777777));
         System.out.println("888888888: " + wordToNumber(888888888));
-    }
-    
-    // O(n) — Linear Time
-    
-    private static String wordToNumber(int number) {
-
-        String word = "";
-
-        int index = 0;
-
-        do {
-
-            int num = number % 1000;
-            if (num != 0) {
-                String singularPlural = num == 1 ? hundreds1[index] : hundreds2[index];
-                word = convertUnderOneThousand(num) + " " + singularPlural + " " + word;
-            }
-            index++;
-            number = number / 1000;
-
-        } while (number > 0);
-
-        return word.substring(0, 1).toUpperCase() + word.substring(1);
-    }
-
-    private static String convertUnderOneThousand(int number) {
-
-        String word = "";
-
-        int index = number % 100;
-
-        if (index < 10) {
-            word += units[index % 10];
-
-        } else if (index < 20) {
-            word += doubles[index % 10];
-
-        } else {
-            if (index % 10 == 0) {
-                word += tens[index / 10];
-            } else {
-                word += tens[index / 10] + "-" + units[index % 10];
-            }
-        }
-
-        int hundred = number / 100;
-
-        return hundred > 0 ? units[hundred] + " hundred " + word : word;
     }
 }
--- src/StringSplitter.java
+++ src/StringSplitter.java
@@ -4,45 +4,32 @@ import java.util.List;
 
 public class StringSplitter {
     
-    public static void main(String[] args) {
-        
-        final List<String> phrase = stringSplitter("Controlling complexity is the essence of computer programming.");
-        
-        for (String word : phrase) {
-            System.out.println(word);
-        }
-    }
-    
     // O(n) — Linear Time
-    
     private static List<String> stringSplitter(String input) {
-        
         if (input == null) {
             return null;
         }
-        
         final String argument = input.trim();
-        
         if (argument.isEmpty()) {
             return Collections.emptyList();
         }
-        
         final List<String> result = new ArrayList<>();
-        
         int start = 0;
-        
         for (int index = 0; index < argument.length(); index++) {
-            
             if (' ' == argument.charAt(index)) {
                 result.add(argument.substring(start, index));
                 start = index + 1;
             }
         }
-        
         result.add(input.substring(start, argument.length()));
-        
         return result;
     }
 
+    public static void main(String[] args) {
+        final List<String> phrase = stringSplitter("Controlling complexity is the essence of computer programming.");
+        for (String word : phrase) {
+            System.out.println(word);
+        }
+    }
 }
 
